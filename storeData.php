4abc36786796e6d0c63aeb9d56b1e0f5f71aac71
Mikael Larsson <a15mikla@student.his.se>
2018-03-12 15:12:46 +0100

    Fixed query in storeData.php
    Fixed the query in storeData.php to actually store the presented JSON
    from JSONgenerator.html

--- storeData.php
+++ storeData.php
@@ -1,5 +1,16 @@
 <?php
-// Work on query!
-$temp = $_POST['json'];
-echo $temp;
+try {
+      $db_conn = new PDO('mysql:host=localhost;dbname=experiment', 'root', '1234');
+      $db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $json = $_POST['json'];
+      $sql = "INSERT INTO dataBLOB (JSONdata) VALUES ('$json')";
+      $db_conn->exec($sql);
+      echo "New record created successfully";
+}
+catch(PDOException $e)
+{
+      echo $sql . "<br>" . $e->getMessage();
+}
+
+$db_conn = null;
 ?>
